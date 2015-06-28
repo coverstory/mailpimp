843dc2e5634bf9d18766581b098fde426479df0a
Aaron O'Mullan <aaron.omullan@gmail.com>
2015-06-28 11:13:38 -0700

    Add static middleware

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+var path = require('path');
 var express = require('express');
 var log = console.log.bind(console);
 
 // Create our app
 var app = express();
 
+// Homepage
 app.get('/', function(req, res) {
     return res.send('hello');
-})
+});
+
+// Static files
+app.use('/static/', express.static(path.join(__dirname, 'public')));
 
 var PORT = process.env.PORT || '3000';
 app.listen(PORT, function(err) {
