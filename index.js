2775091947c9ee5d2c97959f3f92eb444d686cb9
Aaron O'Mullan <aaron.omullan@gmail.com>
2015-06-28 11:36:33 -0700

    Add basic webhook handler

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var express = require('express');
 var log = console.log.bind(console);
+var githubWebhook = require('github-webhook-handler');
 
 // Create our app
 var app = express();
@@ -10,9 +11,30 @@ app.get('/', function(req, res) {
     return res.send('hello');
 });
 
+// Handle webhooks
+var webhook = githubWebhook({ path: '/webhook', secret: 'test', });
+app.post('/webhook', function(req, res) {
+    webhook(req, res, function(err) {
+        res.statusCode = 404;
+        res.end('Webhook error !');
+    })
+})
+
+webhook.on('error', function (err) {
+  console.error('Error:', err.message)
+});
+
+webhook.on('push', function (event) {
+  console.log('Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref)
+});
+
+
 // Static files
 app.use('/static/', express.static(path.join(__dirname, 'public')));
 
+// Listen
 var PORT = process.env.PORT || '3000';
 app.listen(PORT, function(err) {
     if(err) {
@@ -21,4 +43,3 @@ app.listen(PORT, function(err) {
     }
     log('Listening on http://localhost:'+PORT);
 });
-
