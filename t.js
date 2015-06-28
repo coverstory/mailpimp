43ac55484b85ed628338c32338189c632415f5db
Aaron O'Mullan <aaron.omullan@gmail.com>
2015-06-28 13:23:17 -0700

    Test github

--- t.js
+++ t.js
@@ -1,6 +1,10 @@
 var mailchimp = require('./lib/services/mailchimp');
+var github = require('./lib/services/github');
 var log = console.log.bind(console);
 
-mailchimp.createSend("<h1>Chapter test</h1><p>Boom boom</p>", "test.md")
+//mailchimp.createSend("<h1>Chapter test</h1><p>Boom boom</p>", "test.md")
 //mailchimp.lists()
+//.then(log, log);
+
+github.raw('AaronO/mailbook', 'chapter1.md')
 .then(log, log);
